d9c05595db04746231503031be4e194691394a0b
LukeEMS <luke.mallinson@em-solutions.co.uk>
2025-08-16 09:27:24 +0100

    Added basic attributes to the Object

--- objects.py
+++ objects.py
@@ -1,7 +1,16 @@
+from math import pi
 class solar_object:
     def __init__(self, name, mass,radius):
         self.name = name
         self.mass = mass
         self.radius = radius
         self.diameter = 2*radius
-    def 
+        self.volume = (4/3)*pi*(radius^3)
+        self.density = self.mass/self.volume
+
+    def __str__(self):
+        return f"Name: {self.name} Mass: {self.mass}" 
+        
+
+    def gravitational_pull(self,other_object:'solar_object'):
+        ...
