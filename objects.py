aedb003826eb60a1b5d6281efa9c04adde69d065
LukeEMS <luke.mallinson@em-solutions.co.uk>
2025-08-17 08:57:01 +0100

    Added operator overloads to the Vector Class

--- objects.py
+++ objects.py
@@ -8,6 +8,9 @@ class Vector:
         self.x = x
         self.y = y
         self.z = z
+
+    def __str__(self):
+        return f"({self.x},\n{self.y},\n{self.z})"
     
     def __sub__(self,other_vector:'Vector'):
         """
@@ -32,6 +35,49 @@ class Vector:
             self.z + other_vector.z
         )
         return output_vector
+
+    def __pow__(self,exponent:float):
+        """
+        Overload for raising a Vector to the power. 
+        Raise each element to the power of the exponent
+        """
+        output_vector = Vector(
+            self.x**exponent,
+            self.y**exponent,
+            self.z**exponent
+            )
+        return output_vector
+
+    def __mul__(self,multiplier:float):
+        """
+        Overload for multiplying the Vector.
+        Multiplys each element by the multiplier
+        """
+        output_vector = Vector(
+            self.x*multiplier,
+            self.y*multiplier,
+            self.z*multiplier
+            )
+        return output_vector
+
+    def __truediv__(self,divider:float):
+        """
+        Overload for dividing the Vector.
+        Multiplys each element by the divider
+        """
+        output_vector = Vector(
+            self.x/divider,
+            self.y/divider,
+            self.z/divider
+            )
+        return output_vector
+    
+    def __eq__(self,other_vector:'Vector'):
+        if (self.x==other_vector.x) & (self.y == other_vector.y) & (self.z == other_vector.z):
+            return True
+        else:
+            return False
+
     
 class SolarSystem:
     def __init__(self):
--- test.py
+++ test.py
@@ -13,25 +13,37 @@ class VectorTests(unittest.TestCase):
 
     def test_subtracting_two_vectors_returns_correct_results(self):
         # Setup
-        vector = Vector(1,2,3)
-        other_vector = Vector(4,5,6)
-        new_vector = vector + other_vector
+        vector = Vector(4,6,8)
+        other_vector = Vector(1,2,3)
+        new_vector : Vector = vector - other_vector
         # Assert
-        self.assertEqual(5,new_vector.x)
-        self.assertEqual(7,new_vector.y)
-        self.assertEqual(9,new_vector.z)
+        self.assertEqual(Vector(3,4,5),new_vector)
     
     def test_adding_two_vectors_returns_correct_results(self):
         # Setup
         vector = Vector(5,5,5)
         other_vector = Vector(1,2,3)
-        new_vector = vector - other_vector
+        new_vector : Vector = vector + other_vector
         # Assert
-        self.assertEqual(4,new_vector.x)
-        self.assertEqual(3,new_vector.y)
-        self.assertEqual(2,new_vector.z)
+        self.assertEqual(Vector(6,7,8),new_vector)
     
+    def test_multiplying_a_vector(self):
+        # Setup
+        vector = Vector(1,2,3)
+        multiplier = float(2)
+        new_vector : Vector = vector * multiplier
+        # Assert
+        self.assertEqual(Vector(2,4,6),new_vector)
 
+    def test_dividing_a_vector(self):
+        # Setup
+        vector = Vector(6,9,12)
+        divider = float(3)
+        new_vector : Vector = vector/divider
+        # Assert
+        self.assertEqual(2,new_vector.x)
+        self.assertEqual(3,new_vector.y)
+        self.assertEqual(4,new_vector.z)
 
 if __name__ == "__main_":
     unittest.main()
